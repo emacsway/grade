0edea0a494cefae49354efbe6753f0b8524bfa25
Rodion Promyshlennikov <r.promyshlennikov@omp.ru>
2022-07-14 00:44:33 +0300

    Draft: WIP:
    Try to find some aggregates out

--- grade/domain/grade/grade.go
+++ grade/domain/grade/grade.go
@@ -1,4 +1,39 @@
 package grade
 
-type Grade struct {
+import (
+	"errors"
+	"time"
+)
+
+const Begginer = 0
+
+type Recommendation struct {
+	recommended  Member
+	recommending Member
+}
+
+type Testimonial struct {
+	supporter Member
+	promoted  Member
+}
+
+type Member struct {
+	grade Grade
+}
+
+func (m Member) Recommend(other Member, timeNow time.Time) (Testimonial, error) {
+	if m.grade < other.grade {
+		return Testimonial{}, errors.New("cannot support elders")
+	}
+
+	return Testimonial{
+		supporter: m,
+		promoted:  other,
+	}, nil
+}
+
+type Grade int
+
+func (g Grade) LT(other Grade) bool {
+	return g < other
 }
