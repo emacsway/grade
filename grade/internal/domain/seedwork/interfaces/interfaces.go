3426f85fee104cf026aba496ac10fa3b26bc14f2
Ivan Zakrevsky <ivzak@yandex.ru>
2022-07-23 16:07:40 +0300

    Move PublicDomainEvent to buildingblocks since it can't carry ValueObject

--- grade/internal/domain/seedwork/interfaces/interfaces.go
+++ grade/internal/domain/seedwork/interfaces/interfaces.go
@@ -1,7 +1,5 @@
 package interfaces
 
-import "github.com/emacsway/qualifying-grade/grade/pkg/domain/seedwork/interfaces"
-
 type Exporter[T any] interface {
 	SetState(T)
 }
@@ -22,10 +20,7 @@ type Identity[T comparable] interface {
 	Equals(Identity[T]) bool
 }
 
-// Public listeners is a subset of all listeners, not vice versa. Suppose, we have three circles...
-
 type DomainEvent interface {
-	interfaces.PublicDomainEvent
 }
 
 type EventiveEntity interface {
