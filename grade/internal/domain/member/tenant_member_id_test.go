35894de4fe8710f9a65379fa44aafad23f13e295
Ivan Zakrevsky <ivzak@yandex.ru>
2022-07-28 03:53:07 +0300

    Add TestTenantMemberIdEquals

--- grade/internal/domain/member/tenant_member_id_test.go
+++ grade/internal/domain/member/tenant_member_id_test.go
@@ -1,11 +1,44 @@
 package member
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/emacsway/qualifying-grade/grade/internal/domain/seedwork"
 	"github.com/stretchr/testify/assert"
 )
+
+func TestTenantMemberIdEquals(t *testing.T) {
+	cases := []struct {
+		TenantId       uint64
+		MemberId       uint64
+		OtherTenantId  uint64
+		OtherMemberId  uint64
+		ExpectedResult bool
+	}{
+		{1, 2, 1, 2, true},
+		{1, 1, 1, 2, false},
+		{2, 2, 1, 2, false},
+		{1, 2, 1, 1, false},
+		{1, 2, 2, 2, false},
+	}
+	for i, c := range cases {
+		t.Run(fmt.Sprintf("Case %d", i), func(t *testing.T) {
+			id, err := NewTenantMemberId(c.TenantId, c.MemberId)
+			if err != nil {
+				t.Error(err)
+				t.FailNow()
+			}
+			otherId, err := NewTenantMemberId(c.OtherTenantId, c.OtherMemberId)
+			if err != nil {
+				t.Error(err)
+				t.FailNow()
+			}
+			r := id.Equals(otherId)
+			assert.Equal(t, c.ExpectedResult, r)
+		})
+	}
+}
 
 func TestTenantMemberIdExport(t *testing.T) {
 	cid, err := NewTenantMemberId(1, 2)
