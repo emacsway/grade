0c44537992ba9083a7acbf8c7ffa11690b4d2168
Ivan Zakrevsky <ivzak@yandex.ru>
2022-07-31 22:15:40 +0300

    Replace Generic by Uint64Identity

--- grade/internal/domain/seedwork/identity.go
+++ grade/internal/domain/seedwork/identity.go
@@ -18,26 +18,26 @@ type Equaler interface {
 	Equals(Equaler) bool
 }
 
-func NewIdentity[T comparable](value T) (Identity[T, ExporterSetter[T]], error) {
-	return Identity[T, ExporterSetter[T]]{value: value}, nil
+func NewUint64Identity(value uint64) (Uint64Identity, error) {
+	return Uint64Identity{value: value}, nil
 }
 
-// The way to fix issue of generics:
-// https://issuemode.com/issues/golang/go/105227904
-
-type Identity[T comparable, C ExporterSetter[T]] struct {
-	value T
+type Uint64Identity struct {
+	value uint64
 }
 
-func (id Identity[T, C]) Equals(other Equaler) bool {
-	exportableOther := other.(Accessable[T])
+func (id Uint64Identity) Equals(other Equaler) bool {
+	if id.value == 0 {
+		return false // Aggregate is not saved.
+	}
+	exportableOther := other.(Accessable[uint64])
 	return id.value == exportableOther.Value()
 }
 
-func (id Identity[T, C]) Value() T {
+func (id Uint64Identity) Value() uint64 {
 	return id.value
 }
 
-func (id Identity[T, C]) Export(ex C) {
+func (id Uint64Identity) Export(ex ExporterSetter[uint64]) {
 	ex.SetState(id.value)
 }
