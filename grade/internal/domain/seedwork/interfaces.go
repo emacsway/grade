9071e9785dda02325b141b760ed8b5ebc77d5a37
Ivan Zakrevsky <ivzak@yandex.ru>
2022-08-13 18:13:16 +0300

    Implement Session.Atomic()

--- grade/internal/domain/seedwork/interfaces.go
+++ grade/internal/domain/seedwork/interfaces.go
@@ -2,7 +2,4 @@ package seedwork
 
 type Session interface {
 	Atomic(func(Session) error) error
-	/* Begin() (Session, error)
-	Commit() error
-	Rollback() error */
 }
--- grade/internal/infrastructure/pgx_session.go
+++ grade/internal/infrastructure/pgx_session.go
@@ -1,21 +1,53 @@
 package infrastructure
 
 import (
-	"github.com/jackc/pgx/v4"
+	"database/sql"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/pkg/errors"
 
 	"github.com/emacsway/grade/grade/internal/domain/seedwork"
 )
 
-func NewPgxSession(db pgx.Conn) *PgxSession {
+func NewPgxSession(db *sql.DB) *PgxSession {
 	return &PgxSession{
-		db: db,
+		db:         db,
+		dbExecutor: db,
 	}
 }
 
 type PgxSession struct {
-	db pgx.Conn
+	db         *sql.DB
+	dbExecutor DbExecutor
 }
 
-func (s *PgxSession) Atomic(func(session seedwork.Session) error) error {
+func (s *PgxSession) Atomic(callback func(session seedwork.Session) error) error {
+	// TODO: Add support for SavePoint:
+	// https://github.com/golang/go/issues/7898#issuecomment-580080390
+	if s.db == nil {
+		return errors.New("savePoint is not currently supported")
+	}
+	tx, err := s.db.Begin()
+	if err != nil {
+		return errors.Wrap(err, "unable to start transaction")
+	}
+	newSession := &PgxSession{
+		dbExecutor: tx,
+	}
+	err = callback(newSession)
+	if err != nil {
+		if txErr := tx.Rollback(); txErr != nil {
+			return multierror.Append(err, txErr)
+		}
+		return err
+	}
+	if txErr := tx.Commit(); txErr != nil {
+		return errors.Wrap(err, "failed to commit tx")
+	}
 	return nil
+}
+
+type DbExecutor interface {
+	Exec(query string, args ...any) (sql.Result, error)
+	Query(query string, args ...any) (*sql.Rows, error)
 }
