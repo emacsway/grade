07b74f0519b284cf3d764fe926b206ae8950805e
Ivan Zakrevsky <ivzak@yandex.ru>
2022-08-15 20:50:08 +0300

    Add DDL for recognizer, endorsement

--- grade/internal/infrastructure/sql/init.sql
+++ grade/internal/infrastructure/sql/init.sql
@@ -10,3 +10,29 @@ CREATE TABLE event_log (
     CONSTRAINT event_log_event_id_uniq UNIQUE (metadata->>'event_id'),
     CONSTRAINT event_log_pk PRIMARY KEY (tenant_id, stream_type, stream_id, stream_position)
 );
+
+CREATE TABLE recognizer (
+    tenant_id uuid NOT NULL,
+    member_id uuid NOT NULL,
+    grade int8 NOT NULL DEFAULT 0,
+    available_endorsement_count NOT NULL,
+    pending_endorsement_count NOT NULL DEFAULT 0,
+    created_at timestamp with time zone NOT NULL,
+    version int NOT NULL,
+    CONSTRAINT recognizer_pk PRIMARY KEY (tenant_id, member_id)
+);
+
+CREATE TABLE endorsement (
+    tenant_id uuid NOT NULL,
+    specialist_id uuid NOT NULL,
+    specialist_grade int8 NOT NULL DEFAULT 0,
+    specialist_version int NOT NULL,
+    recognizer_id uuid NOT NULL,
+    recognizer_grade int8 NOT NULL DEFAULT 0,
+    recognizer_version int NOT NULL,
+    artifact_id uuid NOT NULL,
+    created_at timestamp with time zone NOT NULL,
+    CONSTRAINT endorsement_uniq UNIQUE (tenant_id, specialist_id, artifact_id, recognizer_id),
+    CONSTRAINT endorsement_uniq UNIQUE (tenant_id, recognizer_id, recognizer_version),
+    CONSTRAINT endorsement_pk PRIMARY KEY (tenant_id, specialist_id, specialist_version)
+);
