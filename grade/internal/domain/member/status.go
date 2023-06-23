6d9e95aff68436ae636a4c6571283ff476cf7069
Ivan Zakrevsky <ivzak@yandex.ru>
2023-06-23 04:04:26 +0300

    Add member.Status

---
+++ grade/internal/domain/member/status.go
@@ -0,0 +1,16 @@
+package member
+
+import (
+	"github.com/emacsway/grade/grade/internal/domain/seedwork/exporters"
+)
+
+type Status uint8
+
+func (s Status) Export(ex exporters.ExporterSetter[uint8]) {
+	ex.SetState(uint8(s))
+}
+
+const (
+	Inactive = Status(0)
+	Active   = Status(1)
+)
