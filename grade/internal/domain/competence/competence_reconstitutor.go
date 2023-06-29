01fbede679782a462ad9451cea916fb1d21bf201
Ivan Zakrevsky <ivzak@yandex.ru>
2023-06-30 01:53:09 +0300

    Add CompetenceReconstitutor

---
+++ grade/internal/domain/competence/competence_reconstitutor.go
@@ -0,0 +1,39 @@
+package competence
+
+import (
+	"time"
+
+	"github.com/emacsway/grade/grade/internal/domain/competence/values"
+	member "github.com/emacsway/grade/grade/internal/domain/member/values"
+	"github.com/emacsway/grade/grade/internal/domain/seedwork/aggregate"
+)
+
+type CompetenceReconstitutor struct {
+	Id        values.TenantCompetenceIdReconstitutor
+	Name      string
+	OwnerId   member.TenantMemberIdReconstitutor
+	CreatedAt time.Time
+	Version   uint
+}
+
+func (r CompetenceReconstitutor) Reconstitute() (*Competence, error) {
+	id, err := r.Id.Reconstitute()
+	if err != nil {
+		return nil, err
+	}
+	name, err := values.NewName(r.Name)
+	if err != nil {
+		return nil, err
+	}
+	ownerId, err := r.OwnerId.Reconstitute()
+	if err != nil {
+		return nil, err
+	}
+	return &Competence{
+		id:                 id,
+		name:               name,
+		ownerId:            ownerId,
+		createdAt:          r.CreatedAt,
+		VersionedAggregate: aggregate.NewVersionedAggregate(r.Version),
+	}, nil
+}
