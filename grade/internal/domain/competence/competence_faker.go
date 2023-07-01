33038cdcc8b9d22ff29b0ad1b463973d47d1ed4f
Ivan Zakrevsky <ivzak@yandex.ru>
2023-07-01 05:38:23 +0300

    Add Repository support to CompetenceFaker

--- grade/internal/domain/competence/competence_faker.go
+++ grade/internal/domain/competence/competence_faker.go
@@ -7,20 +7,42 @@ import (
 	member "github.com/emacsway/grade/grade/internal/domain/member/values"
 )
 
+type CompetenceFakerOption func(*CompetenceFaker)
+
+func WithTenantId(tenantId uint) CompetenceFakerOption {
+	return func(f *CompetenceFaker) {
+		f.Id.TenantId = tenantId
+	}
+}
+
+func WithTransientId() CompetenceFakerOption {
+	return func(f *CompetenceFaker) {
+		f.Id.CompetenceId = 0
+	}
+}
+
+func WithRepository(repo CompetenceRepository) CompetenceFakerOption {
+	return func(f *CompetenceFaker) {
+		f.Repository = repo
+	}
+}
+
 func NewCompetenceFaker() *CompetenceFaker {
 	return &CompetenceFaker{
-		Id:        values.NewTenantCompetenceIdFaker(),
-		Name:      "Name1",
-		OwnerId:   member.NewTenantMemberIdFaker(),
-		CreatedAt: time.Now().Truncate(time.Microsecond),
+		Id:         values.NewTenantCompetenceIdFaker(),
+		Name:       "Name1",
+		OwnerId:    member.NewTenantMemberIdFaker(),
+		CreatedAt:  time.Now().Truncate(time.Microsecond),
+		Repository: CompetenceDummyRepository{},
 	}
 }
 
 type CompetenceFaker struct {
-	Id        values.TenantCompetenceIdFaker
-	Name      string
-	OwnerId   member.TenantMemberIdFaker
-	CreatedAt time.Time
+	Id         values.TenantCompetenceIdFaker
+	Name       string
+	OwnerId    member.TenantMemberIdFaker
+	CreatedAt  time.Time
+	Repository CompetenceRepository
 }
 
 func (f CompetenceFaker) Create() (*Competence, error) {
@@ -39,4 +61,14 @@ func (f CompetenceFaker) Create() (*Competence, error) {
 	return NewCompetence(
 		id, name, owner, f.CreatedAt,
 	)
+}
+
+type CompetenceRepository interface {
+	Insert(*Competence) error
+}
+
+type CompetenceDummyRepository struct{}
+
+func (r CompetenceDummyRepository) Insert(agg *Competence) error {
+	return nil
 }
