62e9b864660675d357dad8b397d758768a6302e0
Ivan Zakrevsky <ivzak@yandex.ru>
2023-08-07 18:28:57 +0300

    Extract PersistentEventGetQuery

--- grade/internal/infrastructure/repositories/artifact/queries/artifact_get_query.go
+++ grade/internal/infrastructure/repositories/artifact/queries/artifact_get_query.go
@@ -2,50 +2,21 @@ package queries
 
 import (
 	"github.com/emacsway/grade/grade/internal/domain/artifact"
-	"github.com/emacsway/grade/grade/internal/domain/artifact/values"
 	"github.com/emacsway/grade/grade/internal/infrastructure"
+	"github.com/emacsway/grade/grade/internal/infrastructure/repositories/seedwork/repository"
 )
 
 type ArtifactGetQuery struct {
-	Id         values.TenantArtifactId
-	streamType string
+	repository.PersistentEventGetQuery
 }
 
-func (q ArtifactGetQuery) sql() string {
-	return `
-		SELECT
-		    tenant_id, stream_type, stream_id, stream_position, event_type, event_version, payload, metadata
-		FROM
-			event_log
-		WHERE
-			tenant_id=$1 AND stream_type=$2 AND stream_id=$3
-		ORDER BY
-			tenant_id, stream_type, stream_id, stream_position`
-}
-func (q ArtifactGetQuery) params() []any {
-	var idExp values.TenantArtifactIdExporter
-	q.Id.Export(&idExp)
-	return []any{idExp.TenantId, q.streamType, idExp.ArtifactId.String()}
-}
-func (q *ArtifactGetQuery) SetStreamType(val string) {
-	q.streamType = val
-}
 func (q *ArtifactGetQuery) Get(s infrastructure.DbSessionQuerier) (*artifact.Artifact, error) {
-	rec := &artifact.ArtifactReconstitutor{}
-	rows, err := s.Query(q.sql(), q.params()...)
+	stream, err := q.Stream(s)
 	if err != nil {
 		return nil, err
 	}
-	defer rows.Close()
-	for rows.Next() {
-		err := rows.Scan() // TODO: implement me
-		if err != nil {
-			return nil, err
-		}
-	}
-	err = rows.Err()
-	if err != nil {
-		return nil, err
+	rec := &artifact.ArtifactReconstitutor{
+		PastEvents: stream,
 	}
 	return rec.Reconstitute()
 }
---
+++ grade/internal/infrastructure/repositories/seedwork/repository/persistent_event_get_query.go
@@ -0,0 +1,45 @@
+package repository
+
+import (
+	"github.com/emacsway/grade/grade/internal/domain/seedwork/aggregate"
+	"github.com/emacsway/grade/grade/internal/infrastructure"
+)
+
+type PersistentEventGetQuery struct {
+	StreamId      StreamId
+	SincePosition uint
+}
+
+func (q PersistentEventGetQuery) sql() string {
+	return `
+		SELECT
+		    tenant_id, stream_type, stream_id, stream_position, event_type, event_version, payload, metadata
+		FROM
+			event_log
+		WHERE
+			tenant_id=$1 AND stream_type=$2 AND stream_id=$3 AND stream_position > $4
+		ORDER BY
+			tenant_id, stream_type, stream_id, stream_position`
+}
+func (q PersistentEventGetQuery) params() []any {
+	return []any{q.StreamId.TenantId(), q.StreamId.StreamType(), q.StreamId.StreamId(), q.SincePosition}
+}
+func (q *PersistentEventGetQuery) Stream(s infrastructure.DbSessionQuerier) ([]aggregate.PersistentDomainEvent, error) {
+	stream := []aggregate.PersistentDomainEvent{}
+	rows, err := s.Query(q.sql(), q.params()...)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		err := rows.Scan() // TODO: implement me
+		if err != nil {
+			return nil, err
+		}
+	}
+	err = rows.Err()
+	if err != nil {
+		return nil, err
+	}
+	return stream, nil
+}
