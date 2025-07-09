e501236df382e6bcc1e5026cff2817a772fc22b2
Ivan Zakrevsky <ivzak@yandex.ru>
2025-07-09 03:48:00 +0300

    Add a comment

--- grade/internal/infrastructure/seedwork/specification/transform_visitor_test.go
+++ grade/internal/infrastructure/seedwork/specification/transform_visitor_test.go
@@ -129,6 +129,9 @@ func (c TestContext) NameByPath(path ...string) (string, error) {
 func (c TestContext) somethingPath(prefix string, path ...string) (string, error) {
 	switch path[0] {
 	case "id":
+		// FIXME: It doesn't work with member_id because this attrite is present on both cases:
+		// before transformation and after transformation
+		// Нам нужно добавить JOIN в Visitor для Collection и создать alias для Item
 		return c.somethingIdPath(prefix, path[1:]...)
 	default:
 		return "", fmt.Errorf("can't get field \"%s\"", path[0])
